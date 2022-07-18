bd19cd9873d66e7fbb61937095688dbc1a6210ee
ericmariot <eric.mariot@betrybe.com>
2022-07-18 16:49:36 -0300

    feat: adiciona comentarios do primeiro encontro

--- Error_Handling/error_handling/lib/error_handling.ex
+++ Error_Handling/error_handling/lib/error_handling.ex
@@ -1,5 +1,22 @@
 defmodule ErrorHandling do
-  def hello do
-    :world
+  def error do raise "Oh no!" end
+  def argument_error do raise ArgumentError, message: "the argument value is invalid" end
+
+  def try_rescue do
+    try do
+      raise "Oh no!"
+    rescue
+      e in RuntimeError -> IO.puts("An error occurred: " <> e.message)
+    end
+  end
+
+  def try_rescue_after do
+    try do
+      raise "Oh no!"
+    rescue
+      e in RuntimeError -> IO.puts("An error occurred: " <> e.message)
+    after
+      IO.puts"The end!"
+    end
   end
 end

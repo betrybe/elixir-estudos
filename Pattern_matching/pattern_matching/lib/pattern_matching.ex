053e10a0d6aa48ade825f4442cdcd4efc4a130e8
Thiago Miarelli <46660124+thiagomiarelli@users.noreply.github.com>
2022-07-19 09:30:22 -0300

    estudos dia 1

--- Pattern_matching/pattern_matching/lib/pattern_matching.ex
+++ Pattern_matching/pattern_matching/lib/pattern_matching.ex
@@ -4,5 +4,13 @@ defmodule PatternMatching do
     :world
   end
 
-  defp teste()
+  def foo(%{name: "katia"}) do
+    IO.inspect("Fala, Katia")
+  end
+
+  def foo(%{name: name}) do
+    IO.inspect("ola, #{name}")
+  end
+  #Como fazer um pattern matching de strings. exemplos:
+  # assert "user 3 not found" = "user #{n} not found"
 end

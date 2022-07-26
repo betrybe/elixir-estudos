7ff0ba1dc8edac7a73a165054d5d29a2e5fd6ce1
Leandro Parazito <leandroh@gmail.com>
2022-07-26 17:04:13 -0300

    estudos dia 2

--- basics/basics/lib/basics.ex
+++ basics/basics/lib/basics.ex
@@ -1,18 +1,20 @@
 defmodule Basics do
-  @moduledoc """
-  Documentation for `Basics`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Basics.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def to_roman(arg) do
+    cond do
+      arg >= 1000 -> "M" <> to_roman(arg - 1000)
+      arg >= 900 -> "CM" <> to_roman(arg - 900)
+      arg >= 500 -> "D" <> to_roman(arg - 500)
+      arg >= 400 -> "CD" <> to_roman(arg - 400)
+      arg >= 100 -> "C" <> to_roman(arg - 100)
+      arg >= 90 -> "XC" <> to_roman(arg - 90)
+      arg >= 50 -> "L" <> to_roman(arg - 50)
+      arg >= 40 -> "XL" <> to_roman(arg - 40)
+      arg >= 10 -> "X" <> to_roman(arg - 10)
+      arg > 8 -> to_roman(abs(arg - 10)) <> "X"
+      arg >= 5 -> "V" <> to_roman(arg - 5)
+      arg > 3 -> to_roman(abs(arg - 5)) <> "V"
+      arg >= 1 -> "I" <> to_roman(arg - 1)
+      arg == 0 -> ""
+    end
   end
 end
--- basics/basics/test/basics_test.exs
+++ basics/basics/test/basics_test.exs
@@ -2,7 +2,52 @@ defmodule BasicsTest do
   use ExUnit.Case
   doctest Basics
 
-  test "greets the world" do
-    assert Basics.hello() == :world
+  test "base types" do
+    base_types = [
+      {1000,"M"},
+      {900,"CM"},
+      {500,"D"},
+      {400,"CD"},
+      {100,"C"},
+      {90,"XC"},
+      {50,"L"},
+      {40,"XL"},
+      {10,"X"},
+      {9,"IX"},
+      {5,"V"},
+      {4,"IV"},
+      {1,"I"},
+    ]
+
+    Enum.each(base_types, fn{i, s} -> assert Basics.to_roman(i) == s end)
+  end
+
+  test "random types" do
+    list = [
+      {1,"I"},
+      {2,"II"},
+      {3,"III"},
+      {4,"IV"},
+      {9,"IX"},
+      {11,"XI"},
+      {12,"XII"},
+      {13,"XIII"},
+      {10,"X"},
+      {20,"XX"},
+      {30,"XXX"},
+      {40,"XL"},
+      {50,"L"},
+      {51,"LI"},
+      {89,"LXXXIX"},
+      {90,"XC"},
+      {100,"C"},
+      {399,"CCCXCIX"},
+      {400,"CD"},
+      {500,"D"},
+      {900,"CM"},
+      {1000,"M"}
+    ]
+
+    Enum.each(list, fn{i, s} -> assert Basics.to_roman(i) == s end)
   end
 end
